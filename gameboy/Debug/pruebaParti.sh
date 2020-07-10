3aa21ca123f2f0a3ab98e5e2750f47773061fc1c
mari <marunk99@gmail.com>
2020-07-09 23:18:06 -0300

    Pruebas particiones dinamicas funcionando, arreglo  +1 en lib

---
+++ gameboy/Debug/pruebaParti.sh
@@ -0,0 +1,10 @@
+./gameboy BROKER GET_POKEMON Pikachu
+./gameboy BROKER GET_POKEMON Charmander
+./gameboy BROKER CATCH_POKEMON Pikachu 9 3
+./gameboy BROKER CATCH_POKEMON Squirtle 9 3
+./gameboy BROKER CAUGHT_POKEMON 10 OK
+./gameboy BROKER CAUGHT_POKEMON 11 FAIL
+./gameboy BROKER CATCH_POKEMON Bulbasaur 1 7
+./gameboy BROKER CATCH_POKEMON Charmander 1 7
+./gameboy BROKER GET_POKEMON Pichu
+./gameboy BROKER GET_POKEMON Raichu
